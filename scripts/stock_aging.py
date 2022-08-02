bc55208f64f89fb42cfaf60c2ce4f17037b58717
AttilaZsamboki <99214950+AttilaZsamboki@users.noreply.github.com>
2022-08-02 12:51:54 +0200

    stock aging

---
+++ scripts/stock_aging.py
@@ -0,0 +1,75 @@
+from datetime import datetime
+from time import strptime
+import pandas as pd
+from sqlalchemy import create_engine
+
+engine = create_engine(
+    "postgresql://doadmin:AVNS_FovmirLSFDui0KIAOnu@db-postgresql-fra1-91708-jun-25-backup-do-user-4907952-0.b.db.ondigitalocean.com:25060/defaultdb?sslmode=require")
+
+obj = pd.read_sql_table(table_name="fol_stock_transaction_report", con=engine)
+obj.sort_values(by="Finished", ascending=False, inplace=True)
+obj.dropna(subset=['Finished'], inplace=True)
+
+skus = {}
+
+for row in obj.iloc:
+    sku = row["Product"]
+    quantity = row["Quantity"]
+    finished = row["Finished"]
+    shippment_supplier_name = row["Shipment_supplier_name"]
+
+    if sku not in skus:
+        skus[sku] = [[quantity, finished, shippment_supplier_name]]
+    else:
+        skus[sku].append([quantity, finished, shippment_supplier_name])
+
+obj2 = pd.read_sql_table(table_name="fol_stock_report", con=engine)
+
+stock_report_obj = {}
+
+for row in obj2.iloc:
+    sku = row["sku"]
+
+    if sku in skus and sku not in stock_report_obj:
+        stock_report_obj[sku] = {"on_stock": row["on_stock"],
+                                 "inventory_value_layer": row["inventory_value_layer"], "on_stock_layer": row["on_stock_layer"]}
+    elif sku in skus and sku in stock_report_obj:
+        stock_report_obj[sku]["on_stock"] = row["on_stock"]
+        stock_report_obj[sku]["inventory_value_layer"] += row["inventory_value_layer"]
+        stock_report_obj[sku]["on_stock_layer"] += row["on_stock_layer"]
+
+for sku, stock_report_info in stock_report_obj.items():
+    skus[sku].append(stock_report_info)
+
+stock_aging = {"sku": [], "shipment": [], "shipment_now": [], "current_stock": [], "date": [],
+               "age": [], "weighted_archimetric_mean": [], "shipment_supplier_name": []}
+
+for sku, infos in skus.items():
+    if sku != '-':
+        current_stock = infos[-1]["on_stock"]
+        inventory_value_layer = infos[-1]["inventory_value_layer"]
+        on_stock_layer = infos[-1]["on_stock_layer"]
+        for i in infos:
+            if len(i) == 3 and type(i) is list:
+                shipment = i[0]
+                shipment_date = datetime.now() - \
+                    datetime.strptime(
+                        str(i[1]), "%Y-%m-%d %H:%M") if str(i[1]) != 'nan' else 'nan'
+                date = datetime.strptime(
+                    str(i[1]), "%Y-%m-%d %H:%M") if str(i[1]) != 'nan' else 'nan'
+                shipment_date = shipment_date.days
+
+                stock_aging["sku"].append(sku)
+                stock_aging["shipment_now"].append(
+                    shipment if shipment < current_stock else current_stock)
+                stock_aging["current_stock"].append(infos[-1]["on_stock"])
+                stock_aging["age"].append(shipment_date)
+                stock_aging["weighted_archimetric_mean"].append(
+                    inventory_value_layer / on_stock_layer if on_stock_layer else 0)
+                stock_aging["date"].append(date)
+                stock_aging["shipment_supplier_name"].append(i[-1])
+                stock_aging["shipment"].append(shipment)
+                current_stock = abs(shipment - current_stock)
+
+df = pd.DataFrame(stock_aging)
+df.to_sql("fol_stock_aging", engine, if_exists="replace")
