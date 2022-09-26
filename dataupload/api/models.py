e4fe45744f520b1d52030a53fc2adf059438e151
AttilaZsamboki <zsamboki.attila.jr@gmail.com>
2022-09-26 06:32:20 +0000

    d

--- dataupload/api/models.py
+++ dataupload/api/models.py
@@ -158,11 +158,14 @@ class DatauploadTableOverview(models.Model):
     db_table = models.CharField(max_length=255)
     verbose_name = models.CharField(max_length=255)
     available_at = models.CharField(max_length=255)
+    email_name = models.CharField(max_length=255)
     id = models.AutoField(primary_key=True)
 
     class Meta:
         managed = False
         db_table = "dataupload_tableoverview"
+
+
 
 
 class DjangoAdminLog(models.Model):
