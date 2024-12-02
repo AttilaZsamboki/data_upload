b76286ff1af752ba6d14b0f33f09d3e68d67815b
AttilaZsamboki <zsamboki.attila.jr@gmail.com>
2024-12-02 17:32:19 +0100

    s

--- dataupload/api/upload_handler.py
+++ dataupload/api/upload_handler.py
@@ -3,29 +3,45 @@ import unidecode
 import psycopg2
 from sqlalchemy import create_engine
 import os
-from .models import DatauploadUploadmodel, DatauploadTabletemplates, DatauploadTableOverview
+from .models import (
+    DatauploadUploadmodel,
+    DatauploadTabletemplates,
+    DatauploadTableOverview,
+)
 from .utils.upload import col_by_dtype
 from json import dumps
 from datetime import date
 from .utils.gmail import send_email, gmail_authenticate
 import dotenv
+
 dotenv.load_dotenv()
 
 service = gmail_authenticate("sajat")
 
 
-def handle_uploaded_file(file, table, table_template, user_id, is_new_table, column_bindings, is_feed, is_email=None, sender_email=None):
+def handle_uploaded_file(
+    file,
+    table,
+    table_template,
+    user_id,
+    is_new_table,
+    column_bindings,
+    is_feed,
+    is_email=None,
+    sender_email=None,
+):
     errors = []
     if not is_new_table:
         skiprows = table_template.skiprows
     else:
         skiprows = 0
     upload_model = DatauploadUploadmodel.objects.get(
-        file=file, table=table, user_id=user_id, status="ready")
+        file=file, table=table, user_id=user_id, status="ready"
+    )
     upload_model.status_description = "Feldolgozás alatt"
     upload_model.status = "under upload"
     upload_model.save()
-    null_cols = [i for i, j in column_bindings.items() if j == '']
+    null_cols = [i for i, j in column_bindings.items() if j == ""]
     for i in null_cols:
         del column_bindings[i]
 
@@ -35,7 +51,7 @@ def handle_uploaded_file(file, table, table_template, user_id, is_new_table, col
         "keepalives": 1,
         "keepalives_idle": 60,
         "keepalives_interval": 10,
-        "keepalives_count": 5
+        "keepalives_count": 5,
     }
 
     DB_HOST = os.environ.get("DB_HOST")
@@ -44,22 +60,39 @@ def handle_uploaded_file(file, table, table_template, user_id, is_new_table, col
     DB_PASS = os.environ.get("DB_PASS")
     DB_PORT = os.environ.get("DB_PORT")
 
-    engine = create_engine("postgresql://"+DB_USER+":"+DB_PASS +
-                           "@"+DB_HOST+":"+DB_PORT+"/"+DB_NAME+"?sslmode=require")
+    engine = create_engine(
+        "postgresql://"
+        + DB_USER
+        + ":"
+        + DB_PASS
+        + "@"
+        + DB_HOST
+        + ":"
+        + DB_PORT
+        + "/"
+        + DB_NAME
+        + "?sslmode=require"
+    )
 
-    conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER,
-                            password=DB_PASS, host=DB_HOST, port=DB_PORT, **keepalive_kwargs)
+    conn = psycopg2.connect(
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASS,
+        host=DB_HOST,
+        port=DB_PORT,
+        **keepalive_kwargs,
+    )
     cur = conn.cursor()
     column_binding_values_str = "".join(column_bindings.values())
 
-    #\\\\\\\\\\\\\\\\\\\\\\\\\ data -->> pandas dataframe ///////////////////////////////////////////#
+    # \\\\\\\\\\\\\\\\\\\\\\\\\ data -->> pandas dataframe ///////////////////////////////////////////#
     df = pd.DataFrame([])
-    if extension_format == '.csv':
+    if extension_format == ".csv":
         df = pd.read_csv(file, skiprows=int(skiprows))
-    elif extension_format == '.tsv':
-        df = pd.read_csv(file, skiprows=int(table_template.skiprows), delimiter='\t')
-    elif extension_format in ['.xls', '.xlsx']:
-        df = pd.read_excel(file, skiprows=int(skiprows), engine='openpyxl')
+    elif extension_format == ".tsv":
+        df = pd.read_csv(file, skiprows=int(table_template.skiprows), delimiter="\t")
+    elif extension_format in [".xls", ".xlsx"]:
+        df = pd.read_excel(file, skiprows=int(skiprows), engine="openpyxl")
     else:
         raise ValueError("Unsupported file format!")
     if df.empty:
@@ -74,17 +107,27 @@ def handle_uploaded_file(file, table, table_template, user_id, is_new_table, col
             df["timestamp"] = filename[-10:]
         column_bindings["timestamp"] = "timestamp"
 
-    if table == 'fol_gls_elszámolás':
-        df = df[df['Súly'].notna()]
-    #\\\\\\\\\\\\\\\\\\\\\\ ADDING NEW TABLES /////////////////////////////////////#
+    if table == "fol_gls_elszámolás":
+        df = df[df["Súly"].notna()]
+    # \\\\\\\\\\\\\\\\\\\\\\ ADDING NEW TABLES /////////////////////////////////////#
 
-    #\\\\\\\\\\\\\\\\\\\\\\\\ UTF-8 >---> ASCII (HEAD) ///////////////////////////////////////#
-    cur.execute("SELECT string_agg(tablename, ', ') FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema';")
+    # \\\\\\\\\\\\\\\\\\\\\\\\ UTF-8 >---> ASCII (HEAD) ///////////////////////////////////////#
+    cur.execute(
+        "SELECT string_agg(tablename, ', ') FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema';"
+    )
     tables_in_sql = list(cur.fetchone())[0].split(", ")
     if is_new_table:
         column_names = {}
-        chars_to_replace = {": ": "_", " ": "_",
-                            "(": "", ")": "", "%": "", ".": "", "\'": "", "-": "_"}
+        chars_to_replace = {
+            ": ": "_",
+            " ": "_",
+            "(": "",
+            ")": "",
+            "%": "",
+            ".": "",
+            "'": "",
+            "-": "_",
+        }
         for i in df.columns:
             column_names[i] = unidecode.unidecode(i)
             for l, j in chars_to_replace.items():
@@ -97,25 +140,35 @@ def handle_uploaded_file(file, table, table_template, user_id, is_new_table, col
             column_bindings = {}
             for i in range(len(db_column_names)):
                 column_bindings[db_column_names[i]] = source_column_names[i]
-            template = DatauploadTabletemplates(table=table, pkey_col="", skiprows=skiprows,
-                                                append="Felülírás", source_column_names=dumps(column_bindings))
+            template = DatauploadTabletemplates(
+                table=table,
+                pkey_col="",
+                skiprows=skiprows,
+                append="Felülírás",
+                source_column_names=dumps(column_bindings),
+            )
             template.save()
             df.to_sql(table, engine, index=False)
             cur.execute(f"TRUNCATE {table}")
             conn.commit()
-            os.remove('/home/atti/googleds/dataupload/media/' + str(file))
+            os.remove("/app/dataupload/media/" + str(file))
             upload_log = DatauploadUploadmodel.objects.get(
-                table=table, file=file, user_id=user_id)
+                table=table, file=file, user_id=user_id
+            )
             upload_log.status = "success"
             upload_log.status_description = "Sikeres létrehozott tábla!"
             upload_log.save()
             table_overview = DatauploadTableOverview(
-                db_table=table, verbose_name=table.title().replace("_", " "), available_at="upload")
+                db_table=table,
+                verbose_name=table.title().replace("_", " "),
+                available_at="upload",
+            )
             table_overview.save()
             return
         else:
             upload_model = DatauploadUploadmodel.objects.get(
-                file=file, table=table, user_id=user_id)
+                file=file, table=table, user_id=user_id
+            )
             upload_model.status_description = f"Tábla már létezik{table}"
             upload_model.status = "error"
             upload_model.save()
@@ -127,39 +180,67 @@ def handle_uploaded_file(file, table, table_template, user_id, is_new_table, col
         elif table.lower() in tables_in_sql:
             table = table.lower()
         else:
-            return print("Hibásan megadott táblanév, nincs ilyen tábla az adatbázisban!")
+            return print(
+                "Hibásan megadott táblanév, nincs ilyen tábla az adatbázisban!"
+            )
 
     if table in tables_in_sql:
-        numeric_cols = col_by_dtype(['decimal', 'numeric', 'real', 'double precision',
-                                    'smallserial', 'serial', 'bigserial', 'money', 'bigint'], table)
+        numeric_cols = col_by_dtype(
+            [
+                "decimal",
+                "numeric",
+                "real",
+                "double precision",
+                "smallserial",
+                "serial",
+                "bigserial",
+                "money",
+                "bigint",
+            ],
+            table,
+        )
         date_cols = col_by_dtype(
-            ['date', 'timestamp', 'timestamp without time zone'], table)
-        numeric_cols_source = [
-            j for i, j in column_bindings.items() if i in numeric_cols] if numeric_cols else []
-        date_cols_source = [
-            j for i, j in column_bindings.items() if i in date_cols] if date_cols else []
+            ["date", "timestamp", "timestamp without time zone"], table
+        )
+        numeric_cols_source = (
+            [j for i, j in column_bindings.items() if i in numeric_cols]
+            if numeric_cols
+            else []
+        )
+        date_cols_source = (
+            [j for i, j in column_bindings.items() if i in date_cols]
+            if date_cols
+            else []
+        )
         for i in df.columns:
             try:
                 if numeric_cols_source and i in numeric_cols_source:
                     lst = []
                     for x in df[i]:
-                        if type(x) == str and ',' in x:
-                            lst.append(x.replace(',', '.'))
+                        if type(x) == str and "," in x:
+                            lst.append(x.replace(",", "."))
                         else:
                             lst.append(x)
                     df[i] = lst
                     df[i] = df[i].astype(float)
                 if date_cols_source and i in date_cols_source:
-                    df[i] = df[i].astype(dtype='datetime64[ns]')
+                    df[i] = df[i].astype(dtype="datetime64[ns]")
             except (ValueError, psycopg2.errors.DatatypeMismatch) as e:
                 upload_model = DatauploadUploadmodel.objects.get(
-                    file=file, table=table, user_id=user_id)
-                upload_model.status_description = f"Egy hiba lépett fel a(z) '{i}' oszlop tartalmát illetően: {e}"
+                    file=file, table=table, user_id=user_id
+                )
+                upload_model.status_description = (
+                    f"Egy hiba lépett fel a(z) '{i}' oszlop tartalmát illetően: {e}"
+                )
                 upload_model.status = "error"
                 upload_model.save()
                 if is_email:
-                    send_email(service, sender_email, "Rossz tábla tartalom",
-                               f"Egy hiba lépett fel a(z) '{i}' oszlop tartalmát illetően: {e}")
+                    send_email(
+                        service,
+                        sender_email,
+                        "Rossz tábla tartalom",
+                        f"Egy hiba lépett fel a(z) '{i}' oszlop tartalmát illetően: {e}",
+                    )
                     os.remove(str(file))
                 return
 
@@ -173,34 +254,56 @@ def handle_uploaded_file(file, table, table_template, user_id, is_new_table, col
         if i not in df.columns:
             print(i + " not in source file, check your template")
             errors.append(
-                f"{i} oszlop nincs benne a fájlban, biztosítsd azt, hogy benne van és próbáld újra")
+                f"{i} oszlop nincs benne a fájlban, biztosítsd azt, hogy benne van és próbáld újra"
+            )
         if errors:
             if is_email:
-                send_email(service, sender_email, "Feltöltésben akadt hibák", f"""Az alábbi hibák akadtak a feltöltésben: {", ".join(errors)}.
+                send_email(
+                    service,
+                    sender_email,
+                    "Feltöltésben akadt hibák",
+                    f"""Az alábbi hibák akadtak a feltöltésben: {", ".join(errors)}.
 
                                 A fenti okok miatt a feltöltés törlésre került.
-                                """)
-
+                                """,
+                )
 
     primary_key_source = table_template.pkey_col
     primary_key_db = "".join(
-        [i for i, j in column_bindings.items() if j == primary_key_source])
-    base_query = "INSERT INTO \""+table+"\" ("+", ".join(["\""+i+"\"" for i in column_bindings.keys(
-    )]) + ") SELECT "+", ".join(["\""+i+"\"" for i in column_bindings.values()])+" FROM temporary" if column_binding_values_str else "INSERT INTO "+table+" SELECT * FROM temporary"
-   
+        [i for i, j in column_bindings.items() if j == primary_key_source]
+    )
+    base_query = (
+        'INSERT INTO "'
+        + table
+        + '" ('
+        + ", ".join(['"' + i + '"' for i in column_bindings.keys()])
+        + ") SELECT "
+        + ", ".join(['"' + i + '"' for i in column_bindings.values()])
+        + " FROM temporary"
+        if column_binding_values_str
+        else "INSERT INTO " + table + " SELECT * FROM temporary"
+    )
 
     if table_template.append == "Felülírás":
-        cur.execute("TRUNCATE \""+table+"\";")
+        cur.execute('TRUNCATE "' + table + '";')
         conn.commit()
     elif table_template.append == "Hozzáfűzés duplikációk szűrésével":
         cur.execute(
-            f"DELETE FROM \"{table}\" WHERE \"{primary_key_db}\" IN (SELECT \"{primary_key_source}\" FROM temporary);")
+            f'DELETE FROM "{table}" WHERE "{primary_key_db}" IN (SELECT "{primary_key_source}" FROM temporary);'
+        )
         conn.commit()
     try:
         if table_template.append == "Változott adat frissítése":
-            set_clause = ", ".join([f"\"{table_col}\" = temporary.\"{temp_col}\"" for table_col, temp_col in column_bindings.items()])
-            column_list_db = ", ".join([f"\"{col}\"" for col in column_bindings.keys()])
-            column_list_temp = ", ".join([f"\"{col}\"" for col in column_bindings.values()])
+            set_clause = ", ".join(
+                [
+                    f'"{table_col}" = temporary."{temp_col}"'
+                    for table_col, temp_col in column_bindings.items()
+                ]
+            )
+            column_list_db = ", ".join([f'"{col}"' for col in column_bindings.keys()])
+            column_list_temp = ", ".join(
+                [f'"{col}"' for col in column_bindings.values()]
+            )
 
             update_query = f"""
                 UPDATE \"{table}\"
@@ -241,19 +344,26 @@ def handle_uploaded_file(file, table, table_template, user_id, is_new_table, col
     conn.close()
 
     upload_model = DatauploadUploadmodel.objects.get(
-        file=file, table=table, user_id=user_id, status="under upload")
+        file=file, table=table, user_id=user_id, status="under upload"
+    )
     upload_model.status_description = "Sikeres feltöltés!"
     upload_model.status = "success"
     file_dir = f"/home/atti/googleds/files/{table}/"
     if not os.path.isdir(file_dir):
         os.mkdir(file_dir)
     if not is_email and not is_feed:
-        files_already_existing = [f for f in os.listdir(
-            file_dir) if f"{date.today()}" in f]
+        files_already_existing = [
+            f for f in os.listdir(file_dir) if f"{date.today()}" in f
+        ]
         upload_model.file = f"{file_dir}{str(filename).split('/')[-1]}{f' ({len(files_already_existing)-1})' if files_already_existing else ''}{extension_format}"
-        os.rename("/home/atti/googleds/dataupload/media/" + str(file),
-                  str(upload_model.file))
+        os.rename(
+            "/home/atti/googleds/dataupload/media/" + str(file), str(upload_model.file)
+        )
     elif is_email:
-        send_email(service, sender_email, "Sikeres feltöltés",
-                   f"Feltöltésed ({filename.split('/')[-1]}) sikeresen felkerült az adatbázisba")
+        send_email(
+            service,
+            sender_email,
+            "Sikeres feltöltés",
+            f"Feltöltésed ({filename.split('/')[-1]}) sikeresen felkerült az adatbázisba",
+        )
     upload_model.save()
