8650c9405ba677861b99939cbd12e012f7ee3e09
AttilaZsamboki <zsamboki.attila.jr@gmail.com>
2024-12-02 17:51:42 +0100

    s

--- dataupload/dataupload/settings.py
+++ dataupload/dataupload/settings.py
@@ -139,7 +139,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "frontend/static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
 
-MEDIAFILES_DIRS = [os.path.join(BASE_DIR, "frontend/media")]
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 
