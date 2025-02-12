905f63bb99952697761bc32ef074a05e1f9bb01d
AttilaZsamboki <zsamboki.attila.jr@gmail.com>
2025-02-12 08:37:36 +0100

    asd

--- dataupload/dataupload/settings.py
+++ dataupload/dataupload/settings.py
@@ -75,11 +75,11 @@ WSGI_APPLICATION = "dataupload.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "defaultdb",
-        "USER": "doadmin",
-        "PASSWORD": "AVNS_FovmirLSFDui0KIAOnu",
-        "HOST": "defaultdb.c0rzdkeutp8f.eu-central-1.rds.amazonaws.com",
-        "PORT": "25060",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASS"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
