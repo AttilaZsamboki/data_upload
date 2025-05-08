b44c03a65a6f3547e9ca0d09304f133b48711769
AttilaZsamboki <zsamboki.attila.jr@gmail.com>
2025-05-08 08:30:41 +0200

    fix: inventory history sales

--- scripts/foliasjuci/inventory_history.py
+++ scripts/foliasjuci/inventory_history.py
@@ -38,7 +38,7 @@ data = pd.read_sql(
         left join fol_stock_report_last stock on stock.sku = unas.cikkszam
         left join (select sku, current_stock, avg(age) age from fol_stock_aging_ext group by 1, 2) aging on unas.cikkszam = aging.sku
         left join (select * from "forgásisebesség_összesítés" where stat_end = (select max(stat_end) from "forgásisebesség_összesítés")) "forgásisebesség_összesítés" on "forgásisebesség_összesítés".sku = unas.cikkszam
-        left join (select * from fol_order_item where "Order_Date" > (select max(time::date) from fol_inventory_history)) as orders on orders."Sku" = unas.cikkszam
+        left join (select * from fol_order_item where "Order_Date" >= (select max(time::date) from fol_inventory_history)) as orders on orders."Sku" = unas.cikkszam
         where "1_alkategoria" in ('Öntapadós fólia',
                 'Ablakfólia',
                 '3D Csempematrica',
